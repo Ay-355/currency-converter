e9730302e9b3632ec368fd3f18db941eb2cbfa45
Ay-355 <77558710+Ay-355@users.noreply.github.com>
2021-02-23 18:15:51 -0700

    Update main_currency_convert.py
    added comments

--- main_currency_convert.py
+++ main_currency_convert.py
@@ -1,28 +1,33 @@
+#! Imports
 import json
 import requests
 
+
+# access API
 r = requests.get('https://openexchangerates.org/api/latest.json?app_id=1410ec879b1c4499a82350698777fb15')
 ex_rates = r.json()
 
-with open('rates.json', 'w') as f:
+with open('rates.json', 'w') as f:      #made json file, not needed
     json.dump(ex_rates, f, indent=4)
 
-rates = ex_rates['rates']
+    
+#! Code
+rates = ex_rates['rates']                   #acess rate part
 
-convertTo = input('What do you want to convert USD to: ')
+convertTo = input('What do you want to convert USD to: ')    #first question
 
 
 for name, value in rates.items():
-    if convertTo in name:
-        valTo = float(input('How much: '))
+    if convertTo in name:                          #checks if input in json file
+        valTo = float(input('How much: '))          #asks how much you want to convert
         valFloat = float(value)
-        result = valTo*valFloat
-        print(f"{valTo} USD is {result} {convertTo}")
-        break
+        result = valTo*valFloat                    #math
+        print(f"{valTo} USD is {result} {convertTo}") 
+        break   
 else:
-    print('That is not a valid input!')
+    print('That is not a valid input!')   #error if don't put correct one in
     print(f"The available rates are:")
-    for name in rates.keys():
+    for name in rates.keys():               #gives available things to convert to
         print(f"{name}")
 
 
