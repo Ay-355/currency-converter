5c17fe988298fb8f54f836e318e309c9c1c93c4c
Ay-355 <77558710+Ay-355@users.noreply.github.com>
2021-02-23 18:11:10 -0700

    Update func_convert.py
    woops need comments, first time doing this lul

--- func_convert.py
+++ func_convert.py
@@ -4,14 +4,14 @@ r = requests.get('https://openexchangerates.org/api/latest.json?app_id=1410ec879
 ex_rates = r.json()
 
 
-rates = ex_rates['rates']
+rates = ex_rates['rates']             #access rates part of json file
 
-def convert(convTo, numInput):
-    for name, value in rates.items():
-        if convTo in name:
-            numInputFloat = float(numInput)
-            result = value*numInputFloat
+def convert(convTo, numInput):                  #define function, currency to convert to and how much we want to convert
+    for name, value in rates.items():           #assigns variables to key, value in file
+        if convTo in name:                      #if input is in the list of available things to convert to
+            numInputFloat = float(numInput)          
+            result = value*numInputFloat        #math
             print(f"{numInputFloat} USD is {result} {convTo}")
             break
 
-convert('INR', 45)
+convert('INR', 45)    #function test
