2494ae620df516a89d64a585ed1706baf62feab5
Ay355 <77558710+Ay-355@users.noreply.github.com>
2021-06-16 18:56:01 -0700

    man was some bad code
    comments were trash, camelCase is not for python smh

--- main_currency_convert.py
+++ main_currency_convert.py
@@ -1,38 +1,20 @@
-#! Imports
 import json
 import requests
 
+js = requests.get('https://openexchangerates.org/api/latest.json?app_id=1410ec879b1c4499a82350698777fb15').json()
 
-# access API
-r = requests.get('https://openexchangerates.org/api/latest.json?app_id=1410ec879b1c4499a82350698777fb15')
-ex_rates = r.json()
+rates = js['rates']
 
-with open('rates.json', 'w') as f:      #made json file, not needed
-    json.dump(ex_rates, f, indent=4)
-
-    
-#! Code
-rates = ex_rates['rates']                   #acess rate part
-
-convertTo = input('What do you want to convert USD to: ')    #first question
-
+convert_to = input('What do you want to convert USD to: ')
 
 for name, value in rates.items():
-    if convertTo in name:                          #checks if input in json file
-        valTo = float(input('How much: '))          #asks how much you want to convert
-        valFloat = float(value)
-        result = valTo*valFloat                    #math
-        print(f"{valTo} USD is {result} {convertTo}") 
+    if convert_to.upper() == name:
+        try:
+            val_to = float(input('How much: '))
+        except ValueError:
+            print("Input has to be a number")
+            break
+        print(f"{val_to} USD is {val_to * float(value)} {convert_to.upper()}")
         break   
 else:
-    print('That is not a valid input!')   #error if don't put correct one in
-    print(f"The available rates are:")
-    for name in rates.keys():               #gives available things to convert to
-        print(f"{name}")
-
-
-
-
-
-
-
+    print("Not a valid input. The available rates are:\n" + ", ".join(name for name in rates.keys()))
