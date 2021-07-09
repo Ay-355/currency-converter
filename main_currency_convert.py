1f5616138666f784ee6958ece04a737dda1bca2f
Ay355 <ItsAy355@gmail.com>
2021-07-08 17:19:28 -0700

    make it an actual cli application... kinda

--- main_currency_convert.py
+++ main_currency_convert.py
@@ -1,20 +1,21 @@
 import json
 import requests
+import sys
 
-js = requests.get('https://openexchangerates.org/api/latest.json?app_id=1410ec879b1c4499a82350698777fb15').json()
+def main():
+    rates = requests.get('https://openexchangerates.org/api/latest.json?app_id=1410ec879b1c4499a82350698777fb15').json()["rates"]
 
-rates = js['rates']
-
-convert_to = input('What do you want to convert USD to: ')
-
-for name, value in rates.items():
-    if convert_to.upper() == name:
+    convert_to = input('What do you want to convert USD to: ')
+    if v := rates.get(convert_to.upper()):
         try:
             val_to = float(input('How much: '))
         except ValueError:
             print("Input has to be a number")
-            break
-        print(f"{val_to} USD is {val_to * float(value)} {convert_to.upper()}")
-        break   
-else:
-    print("Not a valid input. The available rates are:\n" + ", ".join(name for name in rates.keys()))
+            sys.exit(1)
+        print(f"{val_to} USD is {val_to * float(v)} {convert_to.upper()}")
+    else:
+        print("Not a valid input. The available rates are:\n" +", ".join(name for name in rates.keys()))
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
